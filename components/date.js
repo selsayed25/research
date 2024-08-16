a7ca70ea5579a0e269af155904aa39f515d896ed
Sami Elsayed <2025selsayed@tjhsst.edu>
2024-08-15 21:13:32 -0400

    Catch errors related to invalid dates

--- components/date.js
+++ components/date.js
@@ -1,6 +1,14 @@
-import { parseISO, format } from 'date-fns'
+import { parseISO, format, isValid } from 'date-fns'
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString)
+  
+  console.log("Parsed date:", date)
+
+  if (!isValid(date)) {
+    console.error("Invalid date:", dateString)
+    return <time dateTime={dateString}>Invalid date</time>
+  }
+
   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
 }
